964795a8e647aac86164e5c857d146ef74a929d5
mariofer09 <Mariobryant124@gmail.com>
2019-10-03 18:46:13 -0400

    creating date base (Including rankings to the data base)

---
+++ grails-app/init/edu/mario/mg/security/BasicCategory.groovy
@@ -0,0 +1,14 @@
+package edu.mario.mg.security
+
+import edu.mario.mg.model.interfaces.BootStrapInsert
+
+class BasicCategory implements BootStrapInsert {
+
+    BasicCategory() {
+        }
+
+    @Override
+    void insert() {
+
+    }
+}
---
+++ src/test/groovy/edu/mario/mg/RankingSpec.groovy
@@ -0,0 +1,18 @@
+package edu.mario.mg
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class RankingSpec extends Specification implements DomainUnitTest<Ranking> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
